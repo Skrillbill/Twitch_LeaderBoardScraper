b89387343b22762fc5011e9ca4cfa2c6335a7d8f
skrillex <skrill@t3industries.net>
2024-03-06 11:57:13 -0500

    code updates

--- main.py
+++ main.py
@@ -7,10 +7,13 @@ https://github.com/SkrillBill
 from typing import NoReturn
 import configparser
 import shutil
+import pycurl
+import requests
 import wget
 import os
 import logging
 import time
+import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from datetime import datetime
@@ -43,6 +46,31 @@ def update():
     stablerelease = wget.download(url)
     shutil.unpack_archive(stablerelease)
     logging.info('Chromedriver has been updated')
+
+def update_redux():
+    current_version = "0"
+    latest_url = str(update_conf['chromedriver_latest'])
+    if(os.path.isfile('LATEST_STABLE')): # current_version defaults to none/0. But if we've updated before, we'll override the variable with whatvever the last downloaded version was.
+        with open('LATEST_STABLE') as lrs:
+            current_version = lrs.read()
+
+    # google for testing has a json endpoint we can query for the current version of all the release channels. We only care about Stable
+    response = requests.get(str(config['UPDATER_SETTINGS']['chromedriver_version']))
+    data = response.json()
+    new_version = json.dumps(data.get('channels').get('Stable').get('version'))
+    new_version = new_version.strip('"') #strip the quotation marks from the json string
+
+    if str(current_version) == str(new_version) : #is already current version
+        logging.info(f'No update required: Current: {current_version} and new version {new_version}')
+    else: #download new version
+        logging.info(f'New version detected: {new_version}. Installed version: {current_version}')
+        url = str(update_conf['chromedriver_mirror']) + new_version + '/win64/chromedriver-win64.zip'
+        stable_release = wget.download(url)
+        shutil.unpack_archive(stable_release)
+        version_file = open('LATEST_STABLE',"w")
+        version_file.write(new_version)
+        version_file.close()
+        logging.info(f'Chrome driver has been updated to {new_version}')
 
 def scraper():
     # create our headless chrome instance and load the chat page
@@ -97,10 +125,11 @@ def scraper():
 
 def main() -> NoReturn:
     logging.info('Program was initialized')
-    update()
+    update_redux()
+    #update()
 
     #lets get this potty started
-    scraper()
+    #scraper()
 
 
 
