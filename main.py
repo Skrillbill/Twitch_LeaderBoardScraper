afc13af0b056fe27ca323f908b956e5b5bb3b726
skrillex <skrill@t3industries.net>
2024-03-06 12:51:30 -0500

    code updates

--- main.py
+++ main.py
@@ -29,24 +29,6 @@ update_conf = config['UPDATER_SETTINGS']
 #log init
 logging.basicConfig(filename='error.log',encoding='utf-8',filemode='a',level=logging.INFO,format='%(asctime)s: %(levelname)s -> %(message)s')
 
-def update():
-    #should probably change this to actually CHECK the version is new rather than blindly download the same version every day...
-    logging.info('Checking for updates...')
-    url = str(update_conf['chromedriver_latest'])
-    if(os.path.isfile('LATEST_RELEASE_STABLE')):
-        os.remove('LATEST_RELEASE_STABLE')
-        logging.info('removed LATEST_STABLE_RELEASE')
-
-    stableversion = open(wget.download(url),'r').read()
-    url = str(update_conf['chromedriver_mirror']) + stableversion + '/win64/chromedriver-win64.zip'
-
-    if(os.path.isfile('chromedriver-win64.zip')):
-        os.remove('chromedriver-win64.zip')
-        logging.info('removed chromedriver lib')
-    stablerelease = wget.download(url)
-    shutil.unpack_archive(stablerelease)
-    logging.info('Chromedriver has been updated')
-
 def update_redux():
     current_version = "0"
     latest_url = str(update_conf['chromedriver_latest'])
@@ -63,24 +45,25 @@ def update_redux():
     if str(current_version) == str(new_version) : #is already current version
         logging.info(f'No update required: Current: {current_version} and new version {new_version}')
     else: #download new version
-        logging.info(f'New version detected: {new_version}. Installed version: {current_version}')
+        logging.info(f'New version detected: {new_version}. Installed version: {current_version}...Updating')
         url = str(update_conf['chromedriver_mirror']) + new_version + '/win64/chromedriver-win64.zip'
         stable_release = wget.download(url)
         shutil.unpack_archive(stable_release)
+
         version_file = open('LATEST_STABLE',"w")
         version_file.write(new_version)
         version_file.close()
-        logging.info(f'Chrome driver has been updated to {new_version}')
+
+        logging.info(f'Chromedriver has been updated to {new_version}')
 
 def scraper():
     # create our headless chrome instance and load the chat page
     stream_url = config['TWITCH_SETTINGS']['Streamer']
     chrome_options = webdriver.ChromeOptions()
     # chrome_options.add_argument("--headless")
-    init_delay = config['SCRAPER_SETTINGS']['delay_init']
-    screenshot_delay = config['SCRAPER_SETTINGS']['delay_screenshots']
+    init_delay = int(config['SCRAPER_SETTINGS']['delay_init'])
+    screenshot_delay = int(config['SCRAPER_SETTINGS']['delay_screenshots'])
     try:
-        # CHANGE THE EXECUTABLE PATH HERE BELOW THIS LINE(do not need to worry about this i don't think)
         chrome_service = Service('chromedriver-win64\chromedriver.exe')
         driver = webdriver.Chrome(service=chrome_service)
         driver.get(stream_url)
@@ -94,7 +77,6 @@ def scraper():
         gifters = timestamp + "top_gifter.png"
         bittys = timestamp + "top_bittys.png"
 
-        time.sleep(screenshot_delay)
 
         # here we find the rotate leaderboard butotn and click it, expand it, screenshot it, rotate it again, and screenshot again.
         rotate_button = driver.find_element("xpath",
@@ -119,20 +101,20 @@ def scraper():
 
         time.sleep(screenshot_delay)
         driver.quit()
+
     except Exception as err:
+        logging.critical('Something went wrong.. try again or open a ticket at https://github.com/Skrillbill/Twitch_LeaderBoardScraper')
         logging.critical('%s',err)
 
 
+
 def main() -> NoReturn:
-    logging.info('Program was initialized')
-    update_redux()
-    #update()
-
-    #lets get this potty started
-    #scraper()
-
-
-
+    if(os.path.isfile('config.ini')):
+        logging.info('Program was initialized')
+        update_redux()
+        scraper()
+    else:
+        logging.critical('config.ini does not exist. Download it from https://github.com/Skrillbill/Twitch_LeaderBoardScraper')
 
 if __name__ == "__main__":
     main()
